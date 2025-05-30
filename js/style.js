66761ced28e0ac2c9f04c474e124978344c347b3
TejuJamdhade <tejaswinijamdhade2000@gmail.com>
2025-05-30 15:56:37 +0530

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,50 @@
+
+ window.addEventListener('load', () => {
+      // Wait 2s, then hide loader and show para
+      setTimeout(() => {
+        document.querySelector('.pyramid-loader').style.display = "none";
+        const para = document.querySelector('.para');
+        para.style.display = "block";
+
+        // After another 2s, show popup and blur para
+        setTimeout(() => {
+          document.getElementById('popup').style.display = 'block';
+          para.classList.add('blur');
+        }, 2000);
+      }, 2000);
+    });
+
+    // Close popup and remove blur
+    document.getElementById('close-btn').addEventListener('click', () => {
+      document.getElementById('popup').style.display = 'none';
+      document.querySelector('.para').classList.remove('blur');
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  }) 
